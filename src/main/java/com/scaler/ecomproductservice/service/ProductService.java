cf1d41f96f24abec0d0bb19ff630f7a434534255
nishantshaligram <nishant.shaligram@gmail.com>
2024-11-11 01:58:13 +0530

    Product Service getAllProducts and getProductByID return type changed

--- src/main/java/com/scaler/ecomproductservice/service/ProductService.java
+++ src/main/java/com/scaler/ecomproductservice/service/ProductService.java
@@ -3,11 +3,12 @@ package com.scaler.ecomproductservice.service;
 import java.util.List;
 
 import com.scaler.ecomproductservice.dto.ProductListResponseDTO;
+import com.scaler.ecomproductservice.dto.ProductResponseDTO;
 import com.scaler.ecomproductservice.model.Product;
 
 public interface ProductService {
-    ProductListResponseDTO getAllProducts();
-    Product getProductById(int id);
+    List<ProductResponseDTO> getAllProducts();
+    ProductResponseDTO getProductById(int id);
     Product createProduct(Product product);
     Product deleteProduct(int id);
     Product updateProduct(int id, Product updatedProduct);
