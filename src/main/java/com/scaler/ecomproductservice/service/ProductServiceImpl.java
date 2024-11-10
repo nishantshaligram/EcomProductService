6163d5a377a946e0792a6a493d71d0716d3f5d8a
nishantshaligram <nishant.shaligram@gmail.com>
2024-11-11 01:58:37 +0530

    ProductServiceImpl return type for methods fixed

--- src/main/java/com/scaler/ecomproductservice/service/ProductServiceImpl.java
+++ src/main/java/com/scaler/ecomproductservice/service/ProductServiceImpl.java
@@ -5,19 +5,20 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.scaler.ecomproductservice.dto.ProductListResponseDTO;
+import com.scaler.ecomproductservice.dto.ProductResponseDTO;
 import com.scaler.ecomproductservice.model.Product;
 
 @Service("productService")
 public class ProductServiceImpl implements ProductService {
 
     @Override
-    public ProductListResponseDTO getAllProducts() {
+    public List<ProductResponseDTO> getAllProducts() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getAllProducts'");
     }
 
     @Override
-    public Product getProductById(int id) {
+    public ProductResponseDTO getProductById(int id) {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'getProductById'");
     }
