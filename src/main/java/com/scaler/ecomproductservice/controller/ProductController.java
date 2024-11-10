e90597ed3f1482ffa37c83d331a0f6c04ee29e01
nishantshaligram <nishant.shaligram@gmail.com>
2024-11-11 01:59:42 +0530

    Get all product and Get single product controller method implemented

--- src/main/java/com/scaler/ecomproductservice/controller/ProductController.java
+++ src/main/java/com/scaler/ecomproductservice/controller/ProductController.java
@@ -1,34 +1,29 @@
 package com.scaler.ecomproductservice.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import com.scaler.ecomproductservice.dto.ProductListResponseDTO;
-import com.scaler.ecomproductservice.dto.ProductRequestDTO;
-import com.scaler.ecomproductservice.dto.ProductResponseDTO;
-import com.scaler.ecomproductservice.service.ProductService;
 
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.scaler.ecomproductservice.dto.ProductListResponseDTO;
+import com.scaler.ecomproductservice.dto.ProductResponseDTO;
+import com.scaler.ecomproductservice.service.ProductService;
 
 
 @RestController
 public class ProductController {
 
     @Autowired
+    @Qualifier("fakeStoreProductService")
     private ProductService productService;
 
-    public ProductController(RestTemplateBuilder restTemplateBuilder) {
-        this.restTemplateBuilder = restTemplateBuilder;
-    }
     @GetMapping("/products")
-    @Qualifier("fakeStoreProductService")
     public ResponseEntity getAllProducts() {
         /*
             ProductResponseDTO p1 = new ProductResponseDTO();
@@ -50,7 +45,13 @@ public class ProductController {
             List<ProductResponseDTO> products = Arrays.asList(p1, p2);
             return ResponseEntity.ok(products);
         */
-        ProductListResponseDTO response =  productService.getAllProducts();
+        List<ProductResponseDTO> response =  productService.getAllProducts();
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity getProductById(@PathVariable Integer id) {
+        ProductResponseDTO response =  productService.getProductById(1);
         return ResponseEntity.ok(response);
     }
     
