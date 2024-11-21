5789ff1e967c8ad48a8a34e62c14fd6061ae5232
nishantshaligram <nishant.shaligram@gmail.com>
2024-11-21 21:02:31 +0530

    ProdcutController CRUD APIs completed

--- src/main/java/com/scaler/ecomproductservice/controller/ProductController.java
+++ src/main/java/com/scaler/ecomproductservice/controller/ProductController.java
@@ -8,43 +8,31 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import com.scaler.ecomproductservice.dto.ProductListResponseDTO;
+import com.scaler.ecomproductservice.dto.ProductRequestDTO;
 import com.scaler.ecomproductservice.dto.ProductResponseDTO;
 import com.scaler.ecomproductservice.service.ProductService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
 public class ProductController {
 
-    @Autowired
-    @Qualifier("fakeStoreProductService")
-    private ProductService productService;
+    private final ProductService productService; // immutable
+
+    public ProductController(@Qualifier("fakeStoreProductService") ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/products")
     public ResponseEntity getAllProducts() {
-        /*
-            ProductResponseDTO p1 = new ProductResponseDTO();
-            p1.setId(1);
-            p1.setTitle("Iphone 15 pro");
-            p1.setPrice(150000);
-            p1.setImage("www.google.com/images/iphone");
-            p1.setDescription("Overrated phone");
-            p1.setCategory("Mobile Phones");
-
-            ProductResponseDTO p2 = new ProductResponseDTO();
-            p2.setId(2);
-            p2.setTitle("Macbook pro");
-            p2.setPrice(250000);
-            p2.setImage("www.google.com/images/macbook");
-            p2.setDescription("Overrated laptop");
-            p2.setCategory("Laptop");
-
-            List<ProductResponseDTO> products = Arrays.asList(p1, p2);
-            return ResponseEntity.ok(products);
-        */
         List<ProductResponseDTO> response =  productService.getAllProducts();
         return ResponseEntity.ok(response);
     }
@@ -54,5 +42,16 @@ public class ProductController {
         ProductResponseDTO response =  productService.getProductById(1);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/products")
+    public ResponseEntity createProduct(@RequestBody ProductRequestDTO productRequestDTO) {
+       ProductResponseDTO response = productService.createProduct(productRequestDTO);
+       return ResponseEntity.ok(response);
+    }
     
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity deleteProduct(@PathVariable("id") Integer id) {
+        boolean response = productService.deleteProduct(id);
+        return ResponseEntity.ok(response);
+    }
 }
