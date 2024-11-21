cbe3ab75632ccda97a00bffa8404766b6c76e6b7
nishantshaligram <nishant.shaligram@gmail.com>
2024-11-21 21:03:14 +0530

    FakeStore Services CRUD methods completed

--- src/main/java/com/scaler/ecomproductservice/service/FakeStoreProductServiceImpl.java
+++ src/main/java/com/scaler/ecomproductservice/service/FakeStoreProductServiceImpl.java
@@ -10,7 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.scaler.ecomproductservice.dto.ProductListResponseDTO;
+import com.scaler.ecomproductservice.FakeStoreAPIClient;
+import com.scaler.ecomproductservice.dto.ProductRequestDTO;
 import com.scaler.ecomproductservice.dto.ProductResponseDTO;
 import com.scaler.ecomproductservice.model.Product;
 
@@ -18,38 +19,43 @@ import com.scaler.ecomproductservice.model.Product;
 public class FakeStoreProductServiceImpl implements ProductService{
 
     private RestTemplateBuilder  restTemplateBuilder;
+    private FakeStoreAPIClient fakeStoreAPIClient
 
-    public FakeStoreProductServiceImpl(RestTemplateBuilder restTemplateBuilder) {
+    public FakeStoreProductServiceImpl(RestTemplateBuilder restTemplateBuilder, FakeStoreAPIClient fakeStoreAPIClient) {
         this.restTemplateBuilder = restTemplateBuilder;
+        this.fakeStoreAPIClient = fakeStoreAPIClient;
     }
 
     @Override
     public List<ProductResponseDTO> getAllProducts() {
         String getAllProductsURL = "https://fakestoreapi.com/products";
         RestTemplate restTemplate = restTemplateBuilder.build();
-        ResponseEntity<List<ProductResponseDTO>> productResponse = restTemplate.exchange(getAllProductsURL, HttpMethod.GET, null,new ParameterizedTypeReference<List<ProductResponseDTO>>() {});
-        return productResponse.getBody();
-        // return null;
+        ResponseEntity<List<ProductResponseDTO>> response = restTemplate.exchange(getAllProductsURL, HttpMethod.GET, null,new ParameterizedTypeReference<List<ProductResponseDTO>>() {});
+        return response.getBody();
     }
 
     @Override
     public ProductResponseDTO getProductById(int id) {
         String getAllProductsURL = "https://fakestoreapi.com/products/" + id;
         RestTemplate restTemplate = restTemplateBuilder.build();
-        ResponseEntity<ProductResponseDTO> productResponse = restTemplate.getForEntity(getAllProductsURL, ProductResponseDTO.class);
-        return productResponse.getBody();
+        ResponseEntity<ProductResponseDTO> response = restTemplate.getForEntity(getAllProductsURL, ProductResponseDTO.class);
+        return response.getBody();
     }
 
     @Override
-    public Product createProduct(Product product) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createProduct'");
+    public ProductResponseDTO createProduct(ProductRequestDTO product) {
+        String createProductURL = "https://fakestoreapi.com/products/";
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        ResponseEntity<ProductResponseDTO> response = restTemplate.postForEntity( createProductURL, product, ProductResponseDTO.class);
+        return response.getBody();
     }
 
     @Override
-    public Product deleteProduct(int id) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'deleteProduct'");
+    public boolean deleteProduct(int id) {
+        String deleteProductURL = "https://fakestoreapi.com/products/" + id;
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        restTemplate.delete(deleteProductURL);
+        return true;
     }
 
     @Override
