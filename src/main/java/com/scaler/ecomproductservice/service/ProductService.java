89a65dff499c02840dd318108b942a9ae5e13757
nishantshaligram <nishant.shaligram@gmail.com>
2024-11-21 22:57:51 +0530

    Product Service getProductbyID exception throw added

--- src/main/java/com/scaler/ecomproductservice/service/ProductService.java
+++ src/main/java/com/scaler/ecomproductservice/service/ProductService.java
@@ -5,12 +5,13 @@ import java.util.List;
 import com.scaler.ecomproductservice.dto.ProductListResponseDTO;
 import com.scaler.ecomproductservice.dto.ProductRequestDTO;
 import com.scaler.ecomproductservice.dto.ProductResponseDTO;
+import com.scaler.ecomproductservice.exception.ProductNotFoundException;
 import com.scaler.ecomproductservice.model.Product;
 
 public interface ProductService {
     List<ProductResponseDTO> getAllProducts();
-    ProductResponseDTO getProductById(int id);
+    ProductResponseDTO getProductById(int id) throws ProductNotFoundException;
     ProductResponseDTO createProduct(ProductRequestDTO product);
     boolean deleteProduct(int id);
-    Product updateProduct(int id, Product updatedProduct);
+    ProductResponseDTO updateProduct(int id, ProductRequestDTO updatedProduct);
 }
